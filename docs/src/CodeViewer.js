78de269aa96c027eb0acb3decb6621966155a827
Dennis Wilkins <dennis@inlineblock.com>
2016-10-18 10:36:37 -0600

    Add line numbers

--- docs/src/CodeViewer.js
+++ docs/src/CodeViewer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-jsx.min';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
 
 
 export default class CodeViewer extends React.Component {
@@ -16,7 +17,7 @@ export default class CodeViewer extends React.Component {
   renderCode () {
     return (
       <code
-        className="language-jsx code-viewer__syntax"
+        className="language-jsx code-viewer__syntax line-numbers"
         dangerouslySetInnerHTML={{ __html: Prism.highlight(this.props.code, Prism.languages.jsx) || '' }}
       />
     );
